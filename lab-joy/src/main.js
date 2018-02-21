278acfe2d4ac5a6247c5d0aa7d6e3c4f8ca9fab1
strawbee <yuejiao.hou@gmail.com>
2018-02-20 23:49:07 -0800

    still broken

--- lab-joy/src/main.js
+++ lab-joy/src/main.js
@@ -13,6 +13,7 @@ class SearchForm extends React.Component {
             limit: 10,
         };
         this.handleChange = this.handleChange.bind(this);
+        this.handleLimitChange = this.handleLimitChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
@@ -21,18 +22,18 @@ class SearchForm extends React.Component {
     }
 
     handleLimitChange(e) {
-        this.setState({ val: e.target.value });
+        this.setState({ limit: e.target.value });
     }
 
     handleSubmit(e) {
         e.preventDefault();
-        this.props.update_state(this.state.val);
+        this.props.update_state(this.state.val, this.state.limit);
     }
 
     render() {
         return (
             <form className="search-form" onSubmit={this.handleSubmit}>
-                <input type="text" name="reddit-name" value={this.state.val} onChange={this.handleChange} placeholder="Search Reddit" />
+                <input type="text" name="reddit-name" value={this.state.val} onChange={this.handleChange} placeholder="Search" />
                 <input type="number" name="limit" min="1" max="100" value={this.state.limit} onChange={this.handleLimitChange} placeholder="10" />
                 <button type="submit">Search</button>
             </form>
@@ -51,13 +52,14 @@ class Results extends React.Component {
                 {this.props.reddit ?
                     <section className="reddit-data">
                         {console.log(this.props.reddit)}
-                        <h2>{this.props.reddit}</h2>
+                        <h2>{this.props.reddit.data.children}</h2>
                     </section>
                     :
                     undefined}
 
                 {this.props.error ?
                     <section className="reddit-error">
+                        {console.log(this.props.error)};
                         <h2>You broke it.</h2>
                     </section>
                     :
@@ -71,21 +73,21 @@ class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            pokemon: null,
+            reddit: null,
             searchError: null,
         };
         this.searchApi = this.searchApi.bind(this);
         this.updateState = this.updateState.bind(this);
     }
 
-    updateState(name, limit) {
-        this.searchApi(name, limit)
+    updateState(val, limit) {
+        this.searchApi(val, limit)
             .then(res => this.setState({ reddit: res.body, searchError: null }))
             .catch(err => this.setState({ reddit: null, searchError: err }));
     }
 
-    searchApi(name, limit) {
-        return superagent.get(`${API_URL}/${name}.json?limit=${limit}`);
+    searchApi(val, limit) {
+        return superagent.get(`${API_URL}/${val}.json?limit=${limit}`);
     }
 
     render() {
